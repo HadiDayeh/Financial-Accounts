820f0650c14e8eb527750cdf098f1e8778ae9154
Hadi Dayeh <hadi.dayeh@gmail.com>
2022-11-27 10:26:19 +0200

    update

--- Financial-Accounts/asgi.py
+++ Financial-Accounts/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for MyExpenses project.
+ASGI config for Financial-Accounts project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyExpenses.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Financial-Accounts.settings')
 
 application = get_asgi_application()
--- Financial-Accounts/wsgi.py
+++ Financial-Accounts/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for MyExpenses project.
+WSGI config for Financial-Accounts project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyExpenses.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Financial-Accounts.settings')
 
 application = get_wsgi_application()
